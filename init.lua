4c1fbce798e38ae3f1bb475698646b59160c61bc
Hugh Woods <3420970+hughwoods@users.noreply.github.com>
2024-03-22 16:14:14 +0000

    More telescope key bindings

--- init.lua
+++ init.lua
@@ -373,7 +373,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sa', builtin.builtin, { desc = '[S]earch [A]ll telescope options' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>th', builtin.colorscheme, { desc = '[T][h]eme selector' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
