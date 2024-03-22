de5f036c3c57df54f9af986664676ae1b5ebb5c0
Hugh Woods <3420970+hughwoods@users.noreply.github.com>
2024-03-22 16:13:18 +0000

    Add git fugitive plugin

--- init.lua
+++ init.lua
@@ -237,6 +237,7 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+  { 'tpope/vim-fugitive' },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
