74aec3374ef44164cef9dc9cd49a067ade5c657e
Hugh Woods <3420970+hughwoods@users.noreply.github.com>
2024-04-10 22:09:55 +0100

    Add keybinds for netrw and buffer unload

--- init.lua
+++ init.lua
@@ -190,6 +190,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- keybinds for directory and buffer navigation
+vim.keymap.set('n', '<leader>n', vim.cmd.Ex, { desc = 'Open netrw file explorer' })
+vim.keymap.set('n', '<leader>x', vim.cmd.bun, { desc = 'Unload buffer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
