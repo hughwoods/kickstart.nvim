761b6f0466e7fea304a5862bf8a93b0bc16d9f69
hughwoods <8329240+asos-hughwoods@users.noreply.github.com>
2024-05-15 09:48:01 +0100

    re-enable nvim tree

--- lua/custom/plugins/navigation.lua
+++ lua/custom/plugins/navigation.lua
@@ -1,13 +1,21 @@
 local nvimtree = {
   'nvim-tree/nvim-tree.lua',
-  enabled = false,
+  enabled = true,
   version = '*',
   lazy = false,
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('nvim-tree').setup {}
+    require('nvim-tree').setup {
+    view = {
+      width = 40,
+      side = 'right',
+    },
+    filters = {
+      dotfiles = false,
+    },
+  }
     local nvt = require 'nvim-tree.api'
     vim.keymap.set('n', '<leader>e', nvt.tree.focus, { desc = 'Open file [E]xplorer' })
     vim.keymap.set('n', '<C-n>', nvt.tree.toggle, { desc = 'Toggle file explorer' })
